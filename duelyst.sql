de35c454a8a6de898fbfd6ab2cc73ab28aa4d995
vedranb1 <veco444@gmail.com>
2017-12-28 17:42:48 +0100

    .

--- duelyst.sql
+++ duelyst.sql
@@ -5,12 +5,12 @@ use duelyst;
 
 create table klasa(
 sifra int not null primary key auto_increment,
-klasa varchar(50) not null
+naziv varchar(50) not null
 );
 
 create table tip(
 sifra int not null primary key auto_increment,
-tip varchar(50) not null
+naziv varchar(50) not null
 );
 
 create table karta(
@@ -21,5 +21,20 @@ tip int not null,
 ogranicenje int not null
 );
 
+create table korisnik(
+sifra int not null primary key auto_increment,
+username varchar(50) not null,
+email varchar(50) not null
+);
+
+create table deck(
+sifra int not null primary key auto_increment,
+naziv varchar(50) not null,
+korisnik int not null,
+karta int not null
+);
+
 alter table karta add foreign key (klasa) references klasa(sifra);
 alter table karta add foreign key (tip) references tip(sifra);
+alter table deck add foreign key (korisnik) references korisnik(sifra);
+alter table deck add foreign key (karta) references karta(sifra);
