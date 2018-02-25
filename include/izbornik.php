eebc86407d4ac08255ca1a5b959746f9d21d0d98
vedranb1 <33353442+vedranb1@users.noreply.github.com>
2018-02-25 15:23:27 +0100

    Add files via upload

---
+++ include/izbornik.php
@@ -0,0 +1,25 @@
+<div id="header">
+			<div>
+				<a href="index.php" class="logo"><img src="images/logo.png" alt=""></a>
+				<ul id="navigation">
+					<li class="selected">
+						<a href="index.php">Home</a>
+					</li>
+					<li>
+					  <!-- <a href="about.php">About</a>   -->
+					</li>
+					<li class="menu">
+						<a href="projects.php">Projects</a>
+					</li>
+					<li class="menu">
+					<!--	<a href="blog.php">Blog</a>-->
+					</li>
+					<li>
+					<!--	<a href="contact.php">Contact</a>-->
+					</li>
+					<li>
+						<a href="login.php">Login</a>
+					</li>
+				</ul>
+			</div>
+		</div>
---
+++ include/skripte.php
@@ -0,0 +1,5 @@
+<script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>
+    <script src="js/mobile.js" type="text/javascript"></script>
