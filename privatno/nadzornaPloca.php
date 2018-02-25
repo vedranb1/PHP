a6f96151c6ce514cf34c7acc5a6b5dc0a2af123d
vedranb1 <33353442+vedranb1@users.noreply.github.com>
2018-02-25 15:22:12 +0100

    Add files via upload

---
+++ privatno/nadzornaPloca.php
@@ -0,0 +1,15 @@
+<?php include_once 'konfiguracija.php'; ?>
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+<?php include_once 'include/header.php'; ?>
+</head> 
+  <body>
+    <div id="page">
+		<?php include_once 'include/izbornik.php'; ?>
+	
+		<?php include_once 'include/podnozje.php'; ?>
+	</div>
+    <?php include_once 'include/skripte.php'; ?>
+  </body>
+</html>
