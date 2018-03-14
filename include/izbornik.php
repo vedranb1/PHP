b7f5bd72d825eae79dc57b3fe0b6ba76d30b54cb
vedranb1 <33353442+vedranb1@users.noreply.github.com>
2018-03-14 14:25:01 +0100

    Add files via upload

--- include/izbornik.php
+++ include/izbornik.php
@@ -12,13 +12,27 @@
 						<a href="projects.php">Projects</a>
 					</li>
 					<li class="menu">
-					<!--	<a href="blog.php">Blog</a>-->
+						<a href="blog.php">Browse Cards</a> 
 					</li>
 					<li>
-					<!--	<a href="contact.php">Contact</a>-->
+					<!--	<a href="builder.php">Deck Builder</a>-->
 					</li>
 					<li>
-						<a href="login.php">Login</a>
+						<?php if($_SERVER["PHP_SELF"]!==$putanjaAPP . "login.php"): ?>
+						  <div class="top-bar-right">
+						    <ul class="menu">
+						      <li style="width: 100%;"><?php if(!isset($_SESSION[$appID."autoriziran"])): ?>
+							  		<a href="<?php echo $putanjaAPP; ?>login.php" class="button">Prijava</a>
+							  	<?php else: ?>
+							  		<a href="<?php echo $putanjaAPP; ?>logout.php" class="button">Odjava <?php 
+							  		
+							  			echo $_SESSION[$appID."autoriziran"]->username;
+							  			
+							  			?></a>
+							  	<?php endif;?></li>
+    </ul>
+  </div>
+  <?php endif;?>
 					</li>
 				</ul>
 			</div>
--- include/skripte.php
+++ include/skripte.php
@@ -1,5 +1,6 @@
 <script src="js/vendor/jquery.js"></script>
-    <script src="js/vendor/what-input.js"></script>
-    <script src="js/vendor/foundation.js"></script>
-    <script src="js/app.js"></script>
-    <script src="js/mobile.js" type="text/javascript"></script>
+<script src="js/vendor/what-input.js"></script>
+<script src="js/vendor/foundation.js"></script>
+<script src="js/app.js"></script>
+<script src="js/mobile.js" type="text/javascript"></script>
+<script src="js/vendor/fontawesome-all.js"></script>
